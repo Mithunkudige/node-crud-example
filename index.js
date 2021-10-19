89a3d27edb287bbb6475eaaedfa991f27a84b617
Mithun Kudige <mail4mithu@gmail.com>
2021-10-19 21:58:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import bookRoutes from './routes/books.js';
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/', bookRoutes);
+
+app.listen(3000, () => console.log('Listening at post 3000...'));
